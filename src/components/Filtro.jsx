13098f80b79d2c51b7a9753067a1eae3771d8f84
Emanuellk <emanuel.maggiolk@gmail.com>
2022-01-02 20:55:21 -0300

    Subiendo la app de control de gastos hecha en React

---
+++ src/components/Filtro.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+const Filtro = ({filtro, setFiltro}) => {
+    return (
+        <div className='filtros sombra contenedor'>
+            <form className='campo'>
+                <label>Filtrar gastos</label>
+                <select 
+                    value={filtro}
+                    onChange={e => setFiltro(e.target.value)}
+                >
+                       <option value="">-- Todos --</option>
+                       <option value="ahorro">Ahorro</option>
+                       <option value="comida">Comida</option>
+                       <option value="casa">Casa</option>
+                       <option value="gastos">Gastos Vario</option>
+                       <option value="ocio">Ocio</option>
+                       <option value="salud">Salud</option>
+                       <option value="suscripciones">Suscripciones</option>
+                </select>
+            </form>
+        </div>
+    )
+}
+
+export default Filtro
